77339193243ba488cf07f8f67a645b6653a8d729
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2023-01-19 14:20:58 +0100

    sbatch and bigger lstm

--- job.sh
+++ job.sh
@@ -2,13 +2,13 @@
 
 #SBATCH -A es_chatzi
 ##SBATCH -G 1
-#SBATCH -n 100
+#SBATCH -n 50
 #SBATCH --time=150:00:00
 #SBATCH --mem-per-cpu=4096
 #SBATCH --job-name=rllib_main_sbatch
 #SBATCH --output=output_sbatch.txt
 
 source /cluster/apps/local/env2lmod.sh
-module load gcc/8.2.0 cuda/11.3.1 cudnn/8.2.1.32
+##module load gcc/8.2.0 cuda/11.2.2 cudnn/8.1.0.77
 
 python main.py
