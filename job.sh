8fabfb7123b45c66b9a414df0dd089e6f8948d30
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2023-01-20 14:07:25 +0100

    sbatch thread

--- job.sh
+++ job.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #SBATCH -A es_chatzi
-##SBATCH -G 1
+#SBATCH -G 1
 #SBATCH -n 50
-#SBATCH --time=150:00:00
+#SBATCH --time=75:00:00
 #SBATCH --mem-per-cpu=4096
 #SBATCH --job-name=rllib_main_sbatch
 #SBATCH --output=output_sbatch.txt
 
+export OMP_NUM_THREADS=50
+
 source /cluster/apps/local/env2lmod.sh
-##module load gcc/8.2.0 cuda/11.2.2 cudnn/8.1.0.77
+module load gcc/8.2.0 cuda/11.2.2 cudnn/8.1.0.77
 
 python main.py
